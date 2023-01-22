0da31b055407e1e0cd9bf001a31ea199bf1594e9
nelsonmatenda <matendafigueiredo@hotmail.com>
2023-01-22 19:31:25 +0100

    added a featurin that shows the definition of the selected word

--- popup/pop.js
+++ popup/pop.js
@@ -1,6 +1,6 @@
 console.log("from extension popup");
 const myWord = document.querySelector(".myword");
-
+const wordDefinition = document.querySelector(".definition");
 (async () => {
   const [tab] = await chrome.tabs.query({
     active: true,
@@ -9,5 +9,16 @@ const myWord = document.querySelector(".myword");
   const response = await chrome.tabs.sendMessage(tab.id, {
     command: "selection",
   });
+  let url = `https://api.wordnik.com/v4/word.json/${response}/definitions?limit=1&includeRelated=false&sourceDictionaries=all&useCanonical=false&includeTags=false&api_key=YourAPIkey`;
+  const request = new XMLHttpRequest();
+  request.open("GET", url);
+  request.send();
+
+  request.addEventListener("load", function () {
+    const data = JSON.parse(this.responseText);
+    // console.log(this.responseText);
+    wordDefinition.innerHTML = data[0].text;
+  });
+
   myWord.innerHTML = response;
 })();
